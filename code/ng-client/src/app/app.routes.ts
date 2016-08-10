2db28f1230c4849c7665de63fb113c845e25b7a3
Jon DeJong <jon.dejong@gmail.com>
2016-08-09 21:03:27 -0500

    just backing up

--- code/ng-client/src/app/app.routes.ts
+++ code/ng-client/src/app/app.routes.ts
@@ -1,13 +1,15 @@
 import { provideRouter, RouterConfig } from '@angular/router';
 import { NavigationComponent } from './navigation';
 import { ProjectListComponent } from './project-list';
+import {EmployeeListComponent} from './employee-list/employee-list.component';
 
 export const routes: RouterConfig = [
   {
     path: 'home',
     component: NavigationComponent,
     children: [
-      { path: 'projects', component: ProjectListComponent, pathMatch: 'full'}
+      { path: 'projects', component: ProjectListComponent, pathMatch: 'full'},
+      { path: 'employees', component: EmployeeListComponent, pathMatch: 'full'}
     ]
   },
   {
--- code/ng-client/src/app/shared/index.ts
+++ code/ng-client/src/app/shared/index.ts
@@ -2,6 +2,8 @@ export * from './extHttp.service';
 export * from './project.service';
 export * from './Project';
 export * from './login.service'
+export * from './employee.service'
+export * from './Employee'
 
 import {ExtHttp} from './extHttp.service';
 import {LoginService} from './login.service'
