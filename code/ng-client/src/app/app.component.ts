4f9476f70111f432790e1ca38c6ec54a28feb504
Jon DeJong <jon.dejong@gmail.com>
2016-08-09 19:49:42 -0500

    just backing up

--- code/ng-client/src/app/app.component.ts
+++ code/ng-client/src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import {ROUTER_DIRECTIVES} from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  directives: [ROUTER_DIRECTIVES],
+  directives: [],
   styleUrls: ['app.component.scss'],
   templateUrl: 'app.component.html'
 })
