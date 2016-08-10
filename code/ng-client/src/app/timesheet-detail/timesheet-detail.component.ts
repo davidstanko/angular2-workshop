08deb3e31819571a55ba75fdeb50311e6dce789a
Jon DeJong <jon.dejong@gmail.com>
2016-08-10 00:43:53 -0500

    just backing up

--- code/ng-client/src/app/timesheet-detail/timesheet-detail.component.ts
+++ code/ng-client/src/app/timesheet-detail/timesheet-detail.component.ts
@@ -5,7 +5,6 @@ import {MD_CARD_DIRECTIVES} from '@angular2-material/card/card';
 import {MD_BUTTON_DIRECTIVES} from '@angular2-material/button/button';
 import {Router} from '@angular/router';
 import {Timesheet} from '../shared/Timesheet';
-import {TimesheetCompletePipe} from './timesheet-complete.pipe';
 
 @Component({
   selector: 'app-timesheet-detail',
