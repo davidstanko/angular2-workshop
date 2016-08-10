2110d75eae0333e347de6d96601545c5179669ab
Jon DeJong <jon.dejong@gmail.com>
2016-08-09 23:20:32 -0500

    just backing up

--- code/ng-client/src/app/timesheet-detail/timesheet-detail.component.ts
+++ code/ng-client/src/app/timesheet-detail/timesheet-detail.component.ts
@@ -5,14 +5,13 @@ import {MD_CARD_DIRECTIVES} from '@angular2-material/card/card';
 import {MD_BUTTON_DIRECTIVES} from '@angular2-material/button/button';
 import {Router} from '@angular/router';
 import {Timesheet} from '../shared/Timesheet';
-import {TimesheetCompletePipe} from './timesheet-complete.pipe';
 
 @Component({
   selector: 'app-timesheet-detail',
   templateUrl: 'timesheet-detail.component.html',
   styleUrls: ['timesheet-detail.component.scss'],
   directives: [MD_CARD_DIRECTIVES, MD_LIST_DIRECTIVES, MD_BUTTON_DIRECTIVES],
-  pipes: [TimesheetCompletePipe]
+  pipes: []
 })
 export class TimesheetDetailComponent implements OnInit {
 
